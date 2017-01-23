b843dc759b91ffadbf3c3ea60bc1241329983765
Jake Waldner <Jake Waldner>
2017-01-23 00:04:13 -0900

    Jake Waldner edited person 2

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,12 +1,16 @@
 package edu.gatech.oad.antlab.person;
 
+import java.util.Random;
+import java.util.ArrayList;
+import java.lang.StringBuilder;
+
 /**
  *  A simple class for person 2
  *  returns their name and a
  *  modified string 
  *
- * @author Bob
- * @version 1.1
+ * @author Jake Waldner
+ * @version 1.2
  */
 public class Person2 {
     /** Holds the persons real name */
@@ -31,7 +35,29 @@ public class Person2 {
 	 */
 	private String calc(String input) {
 	  //Person 2 put your implementation here
-	  return null;
+		if (input == null) {
+			return null;
+		}
+		String newString = "";
+		Random rand = new Random();
+
+		ArrayList<Integer> shuffle = new ArrayList<>();
+		for (int i = 0; i < input.length(); i ++) {
+			int randomInt = rand.nextInt(input.length());
+			while (shuffle.contains(randomInt)) {
+				randomInt = randomInt + 1;
+				if (randomInt > input.length() - 1) {
+					randomInt = 0;
+				}
+			}
+			shuffle.add(randomInt);
+		}
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < input.length(); i++) {
+			sb.append(input.indexOf(shuffle.remove(i)));
+		}
+
+		return sb.toString();
 	}
 	/**
 	 * Return a string rep of this object
