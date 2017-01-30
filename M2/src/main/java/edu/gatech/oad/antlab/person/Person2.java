da9ea98354fc520961e5541085209590b4ccc218
Thomas Thachil <github@thomasthachil.com>
2017-01-30 15:54:29 -0500

    changed person2.java temporarily and fixed build file

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -7,7 +7,7 @@ import java.lang.StringBuilder;
 /**
  *  A simple class for person 2
  *  returns their name and a
- *  modified string 
+ *  modified string
  *
  * @author Jake Waldner
  * @version 1.2
@@ -38,33 +38,34 @@ public class Person2 {
 		if (input == null) {
 			return null;
 		}
-		String newString = "";
-		Random rand = new Random();
-
-		ArrayList<Integer> shuffle = new ArrayList<>();
-		for (int i = 0; i < input.length(); i ++) {
-			int randomInt = rand.nextInt(input.length());
-			while (shuffle.contains(randomInt)) {
-				randomInt = randomInt + 1;
-				if (randomInt > input.length() - 1) {
-					randomInt = 0;
-				}
-			}
-			shuffle.add(randomInt);
-		}
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < input.length(); i++) {
-			sb.append(input.indexOf(shuffle.remove(i)));
-		}
-
-		return sb.toString();
+		// String newString = "";
+		// Random rand = new Random();
+        //
+		// ArrayList<Integer> shuffle = new ArrayList<>();
+		// for (int i = 0; i < input.length(); i ++) {
+		// 	int randomInt = rand.nextInt(input.length());
+		// 	while (shuffle.contains(randomInt)) {
+		// 		randomInt = randomInt + 1;
+		// 		if (randomInt > input.length() - 1) {
+		// 			randomInt = 0;
+		// 		}
+		// 	}
+		// 	shuffle.add(randomInt);
+		// }
+		// StringBuilder sb = new StringBuilder();
+		// for (int i = 0; i < input.length(); i++) {
+		// 	sb.append(input.indexOf(shuffle.remove(i)));
+		// }
+        //
+		// return sb.toString();
+		return input;
 	}
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
