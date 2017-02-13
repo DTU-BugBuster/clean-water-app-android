cb9de3a6b4aa3922d91163dc42fb2a680a0af7fc
Thomas Thachil <github@thomasthachil.com>
2017-02-13 15:49:40 -0500

    part of M4

---
+++ M4/app/src/main/java/com/example/fourandahalfmen/m4/WelcomeActivity.java
@@ -0,0 +1,32 @@
+package com.example.fourandahalfmen.m4;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import com.example.fourandahalfmen.m4.R;
+import android.widget.Button;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View.OnClickListener;
+import android.content.Intent;
+
+
+public class WelcomeActivity extends Activity implements OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Button mBtn1 = (Button) findViewById(R.id.login_button);
+        mBtn1.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Log.i("clicks","You Clicked B1");
+        Intent i=new Intent(WelcomeActivity.this, LoginActivity.class);
+        startActivity(i);
+    }
+}
