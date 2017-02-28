f194a5dcc8aed83cb65d828f2cc7766dcf5590ea
JakeWaldner <JakeWaldner@users.noreply.github.com>
2017-02-27 16:09:34 -0900

    added button to submit water report input

---
+++ M6/app/src/main/java/com/example/fourandahalfmen/m4/WaterReportInputActivity.java
@@ -0,0 +1,11 @@
+package com.example.fourandahalfmen.m4;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jakew on 2/27/2017.
+ */
+
+public class WaterReportInputActivity extends AppCompatActivity {
+
+}
