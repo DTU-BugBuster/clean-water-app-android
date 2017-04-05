f568367c92eb29831daae25683bc57e506087d33
Shivamp629 <shivamp629@gmail.com>
2017-04-05 14:35:25 -0400

    Unit Tests

--- M10/app/src/test/java/com/example/fourandahalfmen/m4/ShivamPatelTest.java
+++ M10/app/src/test/java/com/example/fourandahalfmen/m4/ShivamPatelTest.java
@@ -1,8 +1,11 @@
 package com.example.fourandahalfmen.m4;
 
 import org.junit.Test;
+import com.example.fourandahalfmen.m4.data.Users;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +13,35 @@ import static org.junit.Assert.assertEquals;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ShivamPatelTest {
+
+    Users shivam = new Users();
+
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+        assertTrue(shivam.setPassword("Hello022!!"));
+        assertFalse(shivam.setPassword("Hello"));
+        assertFalse(shivam.setPassword("1"));
+        assertFalse(shivam.setPassword("hello022!!"));
+        assertTrue(shivam.setPassword("HELLO!!$$"));
+        assertFalse(shivam.setPassword("Hello022+!!"));
+        assertFalse(shivam.setPassword("Hello022-!!"));
+        assertFalse(shivam.setPassword("H1113333%"));
+        assertTrue(shivam.setPassword("H1113333!"));
     }
+
+
+    /**
+     * tests all possible exceptions
+     */
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNull() {
+        shivam.setPassword(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testEmpty() {
+        shivam.setPassword("");
+    }
+
 }
--- M10/app/src/test/java/com/example/fourandahalfmen/m4/VarunBallariTest.java
+++ M10/app/src/test/java/com/example/fourandahalfmen/m4/VarunBallariTest.java
@@ -1,8 +1,12 @@
 package com.example.fourandahalfmen.m4;
 
+import com.example.fourandahalfmen.m4.data.Users;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +14,34 @@ import static org.junit.Assert.assertEquals;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class VarunBallariTest {
+    
+    Users varun = new Users();
+
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+        assertTrue(varun.setPassword("Hello022!!"));
+        assertFalse(varun.setPassword("Hello"));
+        assertFalse(varun.setPassword("1"));
+        assertFalse(varun.setPassword("hello022!!"));
+        assertTrue(varun.setPassword("HELLO!!$$"));
+        assertFalse(varun.setPassword("Hello022+!!"));
+        assertFalse(varun.setPassword("Hello022-!!"));
+        assertFalse(varun.setPassword("H1113333%"));
+        assertTrue(varun.setPassword("H1113333!"));
+    }
+
+
+    /**
+     * tests all possible exceptions
+     */
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNull() {
+        varun.setPassword(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testEmpty() {
+        varun.setPassword("");
     }
 }
