0490c4a5539286ba95fa455cc183ff67eb945696
Varun-Ballari <varun@ballari.com>
2017-04-05 14:35:43 -0400

    User methods

--- M10/app/src/main/java/com/example/fourandahalfmen/m4/data/Users.java
+++ M10/app/src/main/java/com/example/fourandahalfmen/m4/data/Users.java
@@ -51,12 +51,22 @@ public final class Users {
 
     }
 
-    /**
+    /** A method to ensure the password is valid based on our requirements
      *
-     * @param password
-     * @return
+     * @param password password retrieved from user and getting checked if valid
+     * @return a boolean of whether the password passed in is valid or not
      */
     public boolean setPassword(String password) {
+
+        if (password == null) {
+            throw new IllegalArgumentException("Password cannot be null. Please pass in a valid" +
+                    " password.");
+        }
+
+        if (password.equals("")) {
+            throw new IllegalArgumentException("Password field cannot be empty. Please pass in a" +
+                    " valid password.");
+        }
 
         int validSymbols = 0;
         int capitalLetters = 0;
@@ -67,7 +77,7 @@ public final class Users {
         }
         for (int i = 0; i < password.length(); i++) {
             char cur = password.charAt(i);
-            if (cur == '!' || cur == '$' || cur == '#' || cur == "@")  {
+            if (cur == '!' || cur == '$' || cur == '#' || cur == '@')  {
                 validSymbols++;
             } else if (Character.isUpperCase(cur)) {
                 capitalLetters++;
@@ -76,12 +86,88 @@ public final class Users {
             }
         }
 
-        if (validSymbols > 2 && capitalLetters > 0) {
+        if (validSymbols > 0 && capitalLetters > 0) {
             this.password = password;
             return true;
         } else {
             return false;
         }
+
+    }
+
+    /** A method to ensure the username is valid based on our requirements
+     *
+     * @param username username retrieved from user and getting checked if valid
+     * @return a boolean of whether the username passed in is valid or not
+     */
+    public boolean setUsername(String username) {
+        if (username == null) {
+            throw new IllegalArgumentException("Username cannot be null. Please pass in a valid" +
+                    " username.");
+        }
+
+        if (username.equals("")) {
+            throw new IllegalArgumentException("Username field cannot be empty. Please pass in a" +
+                    " valid username.");
+        }
+
+        int capitalLetters = 0;
+
+        if (username.length() < 5) {
+            return false;
+        }
+        for (int i = 0; i < username.length(); i++) {
+            if (!Character.isLetter(username.charAt(i))) {
+                return false;
+            }
+
+            if (Character.isUpperCase(username.charAt(i))) {
+                capitalLetters++;
+            }
+        }
+
+        if (capitalLetters == 2) {
+            this.username = username;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    /** A method to ensure the email is valid based on our requirements
+     *
+     * @param email username retrieved from email and getting checked if valid
+     * @return a boolean of whether the email passed in is valid or not
+     */
+    public boolean setEmail(String email) {
+
+        if (email == null) {
+            throw new IllegalArgumentException("Email cannot be null. Please pass in a valid" +
+                    " email.");
+        }
+
+        if (email.equals("")) {
+            throw new IllegalArgumentException("Email field cannot be empty. Please pass in a" +
+                    " valid email.");
+        }
+
+
+        if (email.length() < 5) {
+            return false;
+        }
+
+        if (!email.contains(".")) {
+            return false;
+        } else if (!email.contains("@")) {
+            return false;
+        } else if (!email.contains("com") && !email.contains("edu") && !email.contains("org") ) {
+            return false;
+        } else {
+            return true;
+        }
+
+
 
     }
 }
